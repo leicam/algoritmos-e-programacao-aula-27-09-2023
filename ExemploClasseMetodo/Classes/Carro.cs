3315f9783a6f86806bc450c5ddeec2b9f9a73c12
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2023-09-27 20:26:28 -0300

    doc: adicionado comentário sobre variaveis de escopo local e global

--- ExemploClasseMetodo/Classes/Carro.cs
+++ ExemploClasseMetodo/Classes/Carro.cs
@@ -6,16 +6,24 @@ using System.Threading.Tasks;
 
 namespace ExemploClasseMetodo.Classes
 {
+    /// <summary>
+    /// exemplo de classe concreta
+    /// </summary>
     internal class Carro
     {
+        /// <summary>
+        /// exemplo de declaração de variáveis
+        /// de escopo global
+        /// </summary>
         //public string _marca;
         //public string _modelo;
-
         private string _marca;
         private string _modelo;
 
         /// <summary>
         /// Método construtor
+        /// contendo as variaveis marca e modelo de escopo local,
+        /// ou seja, existem apenas no método construtor
         /// </summary>
         /// <param name="marca"></param>
         /// <param name="modelo"></param>
