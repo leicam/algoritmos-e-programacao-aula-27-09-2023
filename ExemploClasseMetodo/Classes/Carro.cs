d17a3956fa4dc021e2a1048067675aae4c0af758
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2023-09-27 20:16:17 -0300

    feat: classe concreta, metodo construtor, metodo, modificadores de acesso

---
+++ ExemploClasseMetodo/Classes/Carro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploClasseMetodo.Classes
+{
+    internal class Carro
+    {
+        //public string _marca;
+        //public string _modelo;
+
+        private string _marca;
+        private string _modelo;
+
+        /// <summary>
+        /// Método construtor
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="modelo"></param>
+        public Carro(string marca, string modelo) 
+        {
+            _marca = marca;
+            _modelo = modelo;
+        }
+
+        /// <summary>
+        /// Exemplo de método
+        /// </summary>
+        public void Ligar()
+        {
+            Console.WriteLine($"Carro {_marca} - {_modelo}: Ligado");
+        }
+    }
+}
---
+++ ExemploClasseMetodo/Program.cs
@@ -0,0 +1,24 @@
+﻿using ExemploClasseMetodo.Classes;
+
+namespace ExemploClasseMetodo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro meuCarro;
+            meuCarro = new Carro("VW", "GOL");
+
+            //migrado para o método construtor
+            //meuCarro._marca = "VW";
+            //meuCarro._modelo = "GOL";
+
+            //alterado o modificador de acesso para privado
+            //meuCarro._marca = "FORD";
+
+            meuCarro.Ligar();
+
+            Console.ReadLine();
+        }
+    }
+}
